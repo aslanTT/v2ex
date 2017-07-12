fd56825c4be72104ac7c2d59259a2e841495ca2a
sky111144 <372580869@qq.com>
2017-07-12 21:55:34 +0800

    api: user has been done by promise

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../models/db')
+var db = require('../models/db');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   return res.render('register', {});
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models/db');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('user', { title: 'Express' });
+  var options = {
+    username: req.cookies.username,
+    nodes: [],
+    topics: []
+  };
+  var node = db.table('node').select();
+  var topic = node.then(function (data) {
+    options.nodes = data;
+    return db.table('topic').select();
+  }).catch(function (error) {
+    return res.send(JSON.stringify({'message':'fail'}))
+  });
+  topic.then(function (data) {
+    options.topics = data;
+    return res.render('user', { options: options });
+  }).catch(function (error) {
+    return res.send(JSON.stringify({'message':'fail'}))
+  });
 });
 
 module.exports = router;
