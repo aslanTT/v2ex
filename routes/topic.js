b98a3c8e2a422ad177899d97c728fdd5a63db1d6
sky111144 <372580869@qq.com>
2017-07-13 22:16:56 +0800

    api: topic has been updated by promise

--- routes/topic.js
+++ routes/topic.js
@@ -3,15 +3,14 @@ var router = express.Router();
 var db = require('../models/db');
 /* GET home page. */
 router.get("/:id", function(req, res, next) {
-  var options = {
-    topic: {}
-  };
   db.table('topic').where({
     id: req.params.id
   }).select()
   .then(function (data) {
-    options.topic = data[0];
-    return res.render('topic', { options: options });
+    return res.render('topic', {
+      topic: data[0],
+      req: req
+     });
   }).catch(function (error) {
     return res.send(JSON.stringify({'message':'fail'}))
   });
