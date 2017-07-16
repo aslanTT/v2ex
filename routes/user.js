ef2d75cbeeec5c447900a02cf6ef9ec3988ab11c
sky111144 <372580869@qq.com>
2017-07-16 23:33:09 +0800

    api: fix bug of user

--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,18 @@ var db = require('../models/db');
 
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
+  var options = {
+    req: req
+  };
   db.table('topic').join('user on topic.user_id = user.user_id where topic.user_id = ' + req.params.id).select()
   .then(function (data) {
-    return res.render('user', { req: req, personalTopics: data });
+    options.personalTopics = data;
+    return db.table('user').where({
+      user_id: req.params.id
+    }).select();
+  }).then(function (data) {
+    options.user = data[0]
+    return res.render('user', options);
   });
 });
 
