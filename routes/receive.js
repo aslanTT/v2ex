6aef7514c1abaf45435a46df4df27bf18c7eea7e
sky111144 <372580869@qq.com>
2017-07-17 22:28:11 +0800

    api: pragination of topic has been done

--- routes/receive.js
+++ routes/receive.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var db = require('../models/db');
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  console.log(7);
   res.render('receive', {
     req: req
    });
--- routes/user.js
+++ routes/user.js
@@ -9,13 +9,37 @@ router.get('/:id', function(req, res, next) {
   };
   db.table('topic').join('user on topic.user_id = user.user_id where topic.user_id = ' + req.params.id).select()
   .then(function (data) {
-    options.personalTopics = data;
+    var topics = data;
+    options.len = Math.ceil(topics.length / 5);
+    options.personalTopics = topics.slice(0,5);
     return db.table('user').where({
       user_id: req.params.id
     }).select();
   }).then(function (data) {
-    options.user = data[0]
+    options.user = data[0];
     return res.render('user', options);
+  }).catch(function (error) {
+    console.error(error);
+  });
+});
+
+router.get('/:id/page/:page_id', function (req, res, next) {
+  var options = {
+    req: req
+  };
+  db.table('topic').join('user on topic.user_id = user.user_id where topic.user_id = ' + req.params.id).select()
+  .then(function (data) {
+    var topics = data;
+    options.len = Math.ceil(topics.length / 5);
+    options.personalTopics = topics.slice(5*(req.params.page_id-1),5*req.params.page_id);
+    return db.table('user').where({
+      user_id: req.params.id
+    }).select();
+  }).then(function (data) {
+    options.user = data[0];
+    return res.render('user', options);
+  }).catch(function (error) {
+    console.error(error);
   });
 });
 
