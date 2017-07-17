a9f27ee21477f48a014359d70491bc20217600b1
sky111144 <372580869@qq.com>
2017-07-17 22:38:25 +0800

    api: pagination of index has been done

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 var db = require('../models/db');
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { req: req });
+  var options = { req: req };
+  db.table('topic').join('user on topic.user_id = user.user_id').select()
+  .then(function (data) {
+    var topics = data;
+    options.len = Math.ceil(topics.length / 10);
+    options.topics = topics.slice(0,10);
+    return res.render('index', options);
+  }).catch(function (error) {
+    console.error(error);
+  });
+});
+
+router.get('/page/:page_id', function (req, res, next) {
+  var options = {req: req};
+  db.table('topic').join('user on topic.user_id = user.user_id').select()
+  .then(function (data) {
+    var topics = data;
+    options.len = Math.ceil(topics.length / 10);
+    options.topics = topics.slice(10*(req.params.page_id-1),10*req.params.page_id);
+    return res.render('index', options);
+  }).catch(function (error) {
+    console.error(error);
+  });
 });
 
 module.exports = router;
