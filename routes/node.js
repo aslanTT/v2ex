e809e557d3eae6ed85c84c74c6126ed3ccde3b47
sky111144 <372580869@qq.com>
2017-07-17 22:45:53 +0800

    api: pagination of node has been done

--- routes/node.js
+++ routes/node.js
@@ -14,11 +14,34 @@ router.get('/:id', function (req, res, next) {
     }).select();
   })
   .then(function (data) {
-    options.topics = data;
+    var topics = data;
+    options.len = Math.ceil(topics.length / 10);
+    options.topics = topics.slice(0,10);
     return res.render('node', options);
   }).catch(function (error) {
     console.log(error);
+  });
+});
+
+router.get('/:id/page/:page_id', function (req, res, next) {
+  var options = {req: req};
+  db.table('node').where({
+    id: req.params.id
+  }).select()
+  .then(function (data) {
+    options.node = data[0];
+    return db.table('topic').where({
+      node_id: req.params.id
+    }).select();
   })
+  .then(function (data) {
+    var topics = data;
+    options.len = Math.ceil(topics.length / 10);
+    options.topics = topics.slice(10*(req.params.page_id-1),10*req.params.page_id);
+    return res.render('node', options);
+  }).catch(function (error) {
+    console.log(error);
+  });
 });
 
 module.exports = router;
